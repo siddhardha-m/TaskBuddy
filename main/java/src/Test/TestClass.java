423f4a5b864d810f3380c4a1e887a6d6d2f68e08
Shivtej Saripudi <ssaripud@iu.edu>
2014-09-27 14:56:43 -0400

    [java]: testing

--- main/java/src/Test/TestClass.java
+++ main/java/src/Test/TestClass.java
@@ -4,6 +4,8 @@ public class TestClass {
 	public static void main(String[] args) {
 		System.out.println("Welcome to TaskBuddy site!!!");
 		
+		System.out.println("Hi");
+		
 		System.out.println("Hello User");
 	}
 }
