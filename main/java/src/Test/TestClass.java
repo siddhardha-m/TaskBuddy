c6bd582992a17149998a3377738a06d8c1fcee53
Siddhardha Raju Mandapati <siddhardha.m@outlook.com>
2014-09-27 14:47:30 -0400

    [java]: Testing merge conflicts 2

--- main/java/src/Test/TestClass.java
+++ main/java/src/Test/TestClass.java
@@ -4,6 +4,6 @@ public class TestClass {
 	public static void main(String[] args) {
 		System.out.println("Welcome to TaskBuddy site!!!");
 		
-		System.out.println("Hello");
+		System.out.println("Hello User");
 	}
 }
