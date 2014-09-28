b4ad388de935beff30e1a7050199a7c1f7e4a508
Siddhardha Raju Mandapati <siddhardha.m@outlook.com>
2014-09-28 01:46:22 -0400

    [java]: Controller classes created

---
+++ main/java/src/com/TaskBuddy/Controllers/LoginController.java
@@ -0,0 +1,5 @@
+package com.TaskBuddy.Controllers;
+
+public class LoginController {
+
+}
---
+++ main/java/src/com/TaskBuddy/Main.java
@@ -0,0 +1,9 @@
+package com.TaskBuddy;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	}
+
+}
