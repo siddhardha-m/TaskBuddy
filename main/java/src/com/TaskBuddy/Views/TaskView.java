e6528280ec9b01c9a6d820e03463bcec70d147a4
Saketh Babu Palla <spalla@iu.edu>
2014-10-05 11:18:00 -0400

    [java]: Added getAllTasks() method in TaskView class

--- main/java/src/com/TaskBuddy/Views/TaskView.java
+++ main/java/src/com/TaskBuddy/Views/TaskView.java
@@ -1,10 +1,15 @@
 package com.TaskBuddy.Views;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import com.TaskBuddy.Controllers.TaskController;
+import com.TaskBuddy.Models.Task;
 
 /**
  * @author Siddhardha
@@ -18,5 +23,15 @@ public class TaskView {
 	public TaskView() {
 	}
 	
-	
+	@GET
+	@Produces({ MediaType.APPLICATION_JSON })
+	public ArrayList<Task> getAllTasks(){
+		try{
+			return TaskController.getAllTasks();
+		}catch(SQLException se){
+			System.err.println("Error Message: " + se.getMessage());
+			System.err.println("Error Stacktrace: " + se.getStackTrace().toString());
+			return null;
+		}
+	}	
 }
