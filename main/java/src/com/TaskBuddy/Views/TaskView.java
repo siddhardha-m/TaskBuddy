c3493cd323d3f334c0a66f35d693528e17b47f40
Siddhardha Raju Mandapati <siddhardha.m@outlook.com>
2014-10-06 20:12:13 -0400

    [java]: Added GET POST and PUT method calls in Views

--- main/java/src/com/TaskBuddy/Views/TaskView.java
+++ main/java/src/com/TaskBuddy/Views/TaskView.java
@@ -3,16 +3,21 @@ package com.TaskBuddy.Views;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import org.apache.log4j.Logger;
 
 import com.TaskBuddy.Controllers.TaskController;
+import com.TaskBuddy.Controllers.UserTaskController;
 import com.TaskBuddy.Models.Task;
+import com.TaskBuddy.Models.UserTask;
 
 /**
  * @author Siddhardha
@@ -47,6 +52,78 @@ public class TaskView {
 			
 			return null;
 			
+		}
+	}
+	
+	@GET @Path("user/{userId}")
+	@Produces({ MediaType.APPLICATION_JSON })
+	public static ArrayList<Task> getAllTasksByUserId(@PathParam("userId") int userId) {
+		try {
+			
+			ArrayList<UserTask> userTasksList = UserTaskController.getAllTasksByUserId(userId);
+			
+			ArrayList<Task> tasksList = new ArrayList<Task>();
+			
+			for (UserTask userTaskRow : userTasksList) {
+				tasksList.add(TaskController.getTaskById(userTaskRow.getTaskId()));
+			}
+			
+			return tasksList;
+			
+		} catch (SQLException e) {
+			
+			log.error("Error message: " + e.getMessage());
+			
+			return null;
+			
+		}
+	}
+	
+	@GET @Path("{taskId}")
+	@Produces({ MediaType.APPLICATION_JSON })
+	public static Task getTaskById(@PathParam("taskId") int taskId) {
+		try {
+			
+			return TaskController.getTaskById(taskId);
+			
+		} catch (SQLException e) {
+			
+			log.error("Error message: " + e.getMessage());
+			
+			return null;
+			
+		}
+	}
+	
+	@POST
+	@Consumes({ MediaType.APPLICATION_JSON })
+	@Produces({ MediaType.APPLICATION_JSON })
+	public static boolean insertTask(Task taskRow) {
+		try {
+			
+			return TaskController.save(taskRow);
+			
+		} catch (SQLException e) {
+			
+			log.error("Error message: " + e.getMessage());
+			
+			return false;
+		}
+	}
+	
+	@PUT @Path("{taskId}")
+	@Consumes({ MediaType.APPLICATION_JSON })
+	@Produces({ MediaType.APPLICATION_JSON })
+	public static boolean updateTask(Task taskRow) {
+		try {
+			
+			return TaskController.save(taskRow);
+			
+		} catch (SQLException e) {
+			
+			log.error("Error message: " + e.getMessage());
+			
+			return false;
 		}
 	}
 }
--- main/java/src/com/TaskBuddy/Views/UserView.java
+++ main/java/src/com/TaskBuddy/Views/UserView.java
@@ -3,9 +3,12 @@ package com.TaskBuddy.Views;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -47,6 +50,54 @@ public class UserView {
 			
 			return null;
 			
+		}
+	}
+	
+	@GET @Path("{userId}")
+	@Produces({ MediaType.APPLICATION_JSON })
+	public static User getUserById(@PathParam("userId") int userId) {
+		try {
+			
+			return UserController.getUserById(userId);
+			
+		} catch (SQLException e) {
+			
+			log.error("Error message: " + e.getMessage());
+			
+			return null;
+			
+		}
+	}
+	
+	@POST
+	@Consumes({ MediaType.APPLICATION_JSON })
+	@Produces({ MediaType.APPLICATION_JSON })
+	public static boolean insertUser(User userRow) {
+		try {
+			
+			return UserController.save(userRow);
+			
+		} catch (SQLException e) {
+			
+			log.error("Error message: " + e.getMessage());
+			
+			return false;
+		}
+	}
+	
+	@PUT @Path("{userId}")
+	@Consumes({ MediaType.APPLICATION_JSON })
+	@Produces({ MediaType.APPLICATION_JSON })
+	public static boolean updateUser(User userRow) {
+		try {
+			
+			return UserController.save(userRow);
+			
+		} catch (SQLException e) {
+			
+			log.error("Error message: " + e.getMessage());
+			
+			return false;
 		}
 	}
 }
