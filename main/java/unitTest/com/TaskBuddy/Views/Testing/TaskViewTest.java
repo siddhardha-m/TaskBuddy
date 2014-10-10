efb2a233351da24d8f57728b7388cb28dbacbbdb
Sai Chatterji Vanam <saivanam@iu.edu>
2014-10-09 21:38:30 -0400

    [RestAssured]:Added Unittest for TaskView Class

--- main/java/unitTest/com/TaskBuddy/Views/Testing/TaskViewTest.java
+++ main/java/unitTest/com/TaskBuddy/Views/Testing/TaskViewTest.java
@@ -1,5 +1,27 @@
 package com.TaskBuddy.Views.Testing;
 
-public class TaskViewTest {
+import static com.jayway.restassured.RestAssured.expect;
+import static org.hamcrest.Matchers.equalTo;
 
+import java.util.Date;
+
+import org.junit.Test;
+
+public class TaskViewTest {
+	@Test
+	public void getUserByIdShouldReturnCorrectUser(){
+		expect().
+				body("taskId", equalTo("1")).
+				body("taskTitle", equalTo("Dishwash")).
+				body("taskDescription", equalTo("")).
+				body("taskPointValue", equalTo("")).
+				body("taskCreatedBy", equalTo("2014-10-07 01:02:49.0")).
+				body("taskCreatedDate", equalTo("")).
+				body("taskDueDate", equalTo("")).
+				body("isTaskCompleted", equalTo("")).
+				body("isTaskDeleted", equalTo("false")).
+		when().
+				get("/tasks/1");
+	}
+	
 }
--- main/java/unitTest/com/TaskBuddy/Views/Testing/UserViewTest.java
+++ main/java/unitTest/com/TaskBuddy/Views/Testing/UserViewTest.java
@@ -24,5 +24,4 @@ public class UserViewTest {
 		when().
 				get("/users/1");
 	}
-
 }
